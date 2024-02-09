c38cb1ae6d3a5a6765c1eef900df7972fd396dd6
Edilson Alexandre Cuamba <EdilsonCuamba@gmail.com>
2024-02-10 00:09:41 +0200

    2.7 Created my first Controller for this course not in my life of course.

---
+++ algafood-api/src/main/java/com/eacuamba/dev/algafoodapi/MeuPrimeiroController.java
@@ -0,0 +1,15 @@
+package com.eacuamba.dev.algafoodapi;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller //this annotation tells Spring that this class is a controller and can handle some HTTP request
+public class MeuPrimeiroController {
+
+    @GetMapping("/hello") // this annotation tells Spring that this method will be invoked when a request to /hello and with method GET be made.
+    @ResponseBody // this annotation tells Spring that the response should be put in the body, no view resolvers should be used.
+    public String hello(){
+        return "hello";
+    }
+}
